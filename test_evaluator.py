15b2817d3a962e28abef2467d05f551cd8a93124
Yanai Elazar <yanaiela@gmail.com>
2021-09-22 17:36:27 +0200

    fixed the test evaluator

--- test_evaluator.py
+++ test_evaluator.py
@@ -1,6 +1,6 @@
 import os
 
-from bridging.leaderboard import evaluate
+import evaluate
 import unittest
 import tempfile
 import typing
@@ -11,63 +11,60 @@ class TestAccuracy(unittest.TestCase):
         y = [
             {
                 'prepositions': [[0], [1], [2], [3], [4]],
-                'links': [0, 1, 1, 1, 1],
+                'relations': [0, 1, 1, 1, 1],
             }
         ]
 
         yh = [
             {
                 'predicted_prepositions': [0, 1, 2, 3, 4],
-                'best_prepositions': [1, 1, 2, 3, 4]
             }
         ]
         res = evaluate.evaluate_documents(y, yh)
 
-        self.assertEqual(1.0, res['Labeled-p'])
-        self.assertEqual(1.0, res['Labeled-r'])
-        self.assertEqual(1.0, res['Labeled-f1'])
+        self.assertEqual(1.0, res['labeled_p'])
+        self.assertEqual(1.0, res['labeled_r'])
+        self.assertEqual(1.0, res['labeled_f1'])
 
     def test_EverythingWrong(self):
         y = [
             {
                 'prepositions': [[0], [1], [2], [3], [4]],
-                'links': [0, 1, 1, 1, 1],
+                'relations': [0, 1, 1, 1, 1],
             }
         ]
 
         yh = [
             {
                 'predicted_prepositions': [1, 0, 0, 0, 0],
-                'best_prepositions': [1, 2, 3, 4, 5]
             }
         ]
 
         res = evaluate.evaluate_documents(y, yh)
 
-        self.assertEqual(0.0, res['Labeled-p'])
-        self.assertEqual(0.0, res['Labeled-r'])
-        self.assertEqual(0.0, res['Labeled-f1'])
+        self.assertEqual(0.0, res['labeled_p'])
+        self.assertEqual(0.0, res['labeled_r'])
+        self.assertEqual(0.0, res['labeled_f1'])
 
     def test_MixedResults(self):
         y = [
             {
                 'prepositions': [[0], [1], [2], [3], [4]],
-                'links': [0, 1, 1, 1, 1],
+                'relations': [0, 1, 1, 1, 1],
             }
         ]
 
         yh = [
             {
                 'predicted_prepositions': [1, 0, 1, 3, 1],
-                'best_prepositions': [1, 2, 3, 4, 5]
             }
         ]
 
         res = evaluate.evaluate_documents(y, yh)
 
-        self.assertEqual(0.25, res['Labeled-p'])
-        self.assertEqual(0.25, res['Labeled-r'])
-        self.assertEqual(0.25, res['Labeled-f1'])
+        self.assertEqual(0.25, res['labeled_p'])
+        self.assertEqual(0.25, res['labeled_r'])
+        self.assertEqual(0.25, res['labeled_f1'])
 
 
 def temp_file_with_contents(lines: typing.List[str]) -> str:
